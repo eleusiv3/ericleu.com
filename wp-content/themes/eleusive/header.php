8e6d8986314cf4f3befa8951242ea69b62e5d042
eleu <eric.leu.2@gmail.com>
2015-07-22 02:50:17 -0400

    added stylesheets to header.php

--- wp-content/themes/eleusive/header.php
+++ wp-content/themes/eleusive/header.php
@@ -15,6 +15,12 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
+<link type="text/css" rel="stylesheet" href="materialize-src/css/materialize.min.css"  media="screen,projection"/>
+
+<!--Import jQuery before materialize.js-->
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="materialize-src/js/bin/materialize.min.js"></script>
+
 <?php wp_head(); ?>
 </head>
 
